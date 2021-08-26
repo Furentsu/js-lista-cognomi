4dd0d7f54fd2f86e1283fa6be790d0c0763fb65f
Furentsu <eugeniofranzoso@gmail.com>
2021-08-26 14:34:55 +0200

    Added a solution

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
-// Consegna:
-// chiedi all’utente il cognome
-// inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
-// stampa la lista ordinata alfabeticamente
-// scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+let surnames = ["Piazza", "Pagani", "Ferrari", "Alemanno", "Lazzaroni", "Bompiani"]; 
+
+let userSurname = prompt("Please, tell us your surname.");
+
+// First letter only must be uppercase ---
+if (userSurname.charAt(0) !== userSurname.charAt(0).toUpperCase()) {
+    alert("You must type the first letter ONLY in uppercase. Reload the page to try again.");
+} else {
+    surnames.push(userSurname);
+}
+
+// Sorting the array
+let orderedSurnames = surnames.sort();
+
+// Adding the elements of the ordered array to the list 
+for (i = 0; i < orderedSurnames.length; i++) {
+   let startList = document.getElementById("list").innerHTML;
+   document.getElementById("list").innerHTML = startList + "<li>" + orderedSurnames[i] + "</li>";
+}
+
 
