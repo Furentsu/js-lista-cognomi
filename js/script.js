cbd0b5fd3f6e8fde656262f05fbf25c079ebba46
Furentsu <eugeniofranzoso@gmail.com>
2021-08-26 16:51:44 +0200

    Added another possible solution

--- js/script.js
+++ js/script.js
@@ -1,12 +1,44 @@
+// SOLUTION #1
+
+// let surnames = ["Piazza", "Pagani", "Ferrari", "Alemanno", "Lazzaroni", "Bompiani"]; 
+
+// let userSurname = prompt("Please, tell us your surname.");
+
+// // First letter only must be uppercase ---
+// if (userSurname.charAt(0) !== userSurname.charAt(0).toUpperCase()) {
+//     alert("You must type the first letter ONLY in uppercase. Reload the page to try again.");
+// } else {
+//     surnames.push(userSurname);
+// }
+
+// // Sorting the array
+// let orderedSurnames = surnames.sort();
+
+// // Adding the elements of the ordered array to the list 
+// for (i = 0; i < orderedSurnames.length; i++) {
+//    let startList = document.getElementById("list").innerHTML;
+//    document.getElementById("list").innerHTML = startList + "<li>" + orderedSurnames[i] + "</li>";
+// }
+
+
+// SOLUTION #2
+
 let surnames = ["Piazza", "Pagani", "Ferrari", "Alemanno", "Lazzaroni", "Bompiani"]; 
 
-let userSurname = prompt("Please, tell us your surname.");
+let userSurname;
 
-// First letter only must be uppercase ---
+// Prompting the user until I get a string ---
+do {
+    userSurname = prompt("Please, tell us your surname. Only letters are going to be accepted!");
+} while (!isNaN(parseInt(userSurname)));
+
+
+// First letter ONLY must be uppercase ---
 if (userSurname.charAt(0) !== userSurname.charAt(0).toUpperCase()) {
-    alert("You must type the first letter ONLY in uppercase. Reload the page to try again.");
-} else {
-    surnames.push(userSurname);
+    alert("You should have typed only the first letter in uppercase. I'll make it work for you.");
+    surnames.push(userSurname.charAt(0).toUpperCase() + userSurname.slice(1).toLowerCase());
+} else if (userSurname.slice(1) !== userSurname.slice(1).toLowerCase()) {
+    surnames.push(userSurname.charAt(0) + userSurname.slice(1).toLowerCase());
 }
 
 // Sorting the array
@@ -14,8 +46,5 @@ let orderedSurnames = surnames.sort();
 
 // Adding the elements of the ordered array to the list 
 for (i = 0; i < orderedSurnames.length; i++) {
-   let startList = document.getElementById("list").innerHTML;
-   document.getElementById("list").innerHTML = startList + "<li>" + orderedSurnames[i] + "</li>";
+   document.getElementById("list").innerHTML += "<li>" + orderedSurnames[i] + "</li>";
 }
-
-
